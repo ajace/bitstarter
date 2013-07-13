327f9092f9090153fc74c35e8dd1b1eacbdde66b
Ace Atienza <aceatienza@about.me>
2013-07-13 18:05:14 +0000

    update author: :wq
    :wq
    i
    helQse enter the commit message for your changes. Lines starting

--- web.js
+++ web.js
@@ -1,10 +1,12 @@
 var express = require('express');
-var content = String(fs.readFileSync('index.html'));
+var content = fs.readFileSync('index.html');
+var buffer = new Buffer(32);
+buffer.write(content);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(content);
+  response.send(buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
