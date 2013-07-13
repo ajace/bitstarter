392d5da02cd87cca4f59a92d564df63bc2657d07
Ubuntu <ubuntu@ip-10-164-18-10.ec2.internal>
2013-07-13 17:59:14 +0000

    readFileSync on index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var content = String(fs.readFileSync('index.html'));
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('index.html');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
